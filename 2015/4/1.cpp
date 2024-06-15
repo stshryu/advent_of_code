bce29050c60dc16cd2c2691b57d8905e23ec623a
Steve <stshryu@gmail.com>
2024-06-15 00:39:25 -0400

    Adding day4 p1p2

---
+++ 2015/4/1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "md5.cpp"
+
+using namespace std;
+
+vector<string> read_file() {
+    string filename = "input.txt";
+    vector<string> data;
+
+    string line;
+    ifstream file(filename);
+    if (file.is_open()) {
+        while(getline(file, line)) {
+            data.push_back(line);
+        }
+    }
+    return data;
+}
+
+int find_zero(string key) {
+    string subhash;
+    int counter = 0;
+    while(subhash != "00000") {
+        counter++;
+        subhash = MD5(key + to_string(counter)).hexdigest().substr(0,5);
+    }
+
+    return counter;
+}
+
+int main() {
+    string data = read_file()[0];
+    data = "abcdef";
+
+    cout << find_zero(data) << '\n';
+
+    return 0;
+}
---
+++ 2015/4/2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "md5.cpp"
+
+using namespace std;
+
+vector<string> read_file() {
+    string filename = "input.txt";
+    vector<string> data;
+
+    string line;
+    ifstream file(filename);
+    if (file.is_open()) {
+        while(getline(file, line)) {
+            data.push_back(line);
+        }
+    }
+    return data;
+}
+
+int find_zero(string key) {
+    string subhash;
+    int counter = 0;
+    while(subhash != "000000") {
+        counter++;
+        subhash = MD5(key + to_string(counter)).hexdigest().substr(0,6);
+    }
+
+    return counter;
+}
+
+int main() {
+    string data = read_file()[0];
+
+    cout << find_zero(data) << '\n';
+
+    return 0;
+}
